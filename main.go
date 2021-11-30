b45989b3c4d9aa8919a8625bba0db9808cb901b9
Anthony Johnson <anthonythomasjohnson@outlook.com>
2021-11-30 15:03:59 +0900

    initial commit

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+// Create the function fuelGauge() here
+
+
+// Create the function calculateFuel() here
+
+
+// Create the function greetPlanet() here
+
+
+// Create the function cantFly() here
+
+
+// Create the function flyToPlanet() here
+
+
+func main() {
+  // Test your functions!
+  
+  // Create `planetChoice` and `fuel`
+  
+  // And then liftoff!
+  
+}
+
+fmt.Println("test")
