33c28ddcd09d040b3bdd4aa14c831c16243d8f37
Anthony Johnson <anthonythomasjohnson@outlook.com>
2021-11-30 15:08:04 +0900

    created fuel guage function

--- main.go
+++ main.go
@@ -3,27 +3,23 @@ package main
 import "fmt"
 
 // Create the function fuelGauge() here
-
+func fuelGauge(fuel int) {
+	fmt.Println("Remaining Fuel:", fuel, "Litres")
+}
 
 // Create the function calculateFuel() here
 
-
 // Create the function greetPlanet() here
-
 
 // Create the function cantFly() here
 
-
 // Create the function flyToPlanet() here
 
-
 func main() {
-  // Test your functions!
-  
-  // Create `planetChoice` and `fuel`
-  
-  // And then liftoff!
-  
-}
+	// Test your functions!
+	fuelGauge(86)
+	// Create `planetChoice` and `fuel`
 
-fmt.Println("test")
+	// And then liftoff!
+
+}
